e6d374cc22907652bc0a29e4b5476a2d8d39bdac
zhwang <516275397@qq.com>
2016-06-12 11:03:02 +0800

    初始化程序入口

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
+var path = require('path');
+var config = require('./config');
 var express = require('express');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(config.port, function (err) {
+    if(err){
+        return console.error(err);
+    }
+
+    console.log('server success port ' + config.port);
+});
+
+// 捕捉异常
+process.on('uncaughtException', function (err) {
+    console.error(err);
+});
